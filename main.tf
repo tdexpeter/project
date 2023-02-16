194616376a9b7a111dd51c33414989504bb1bd27
tdexpeter <117410568+tdexpeter@users.noreply.github.com>
2023-02-16 01:15:09 -0600

    push 6

--- main.tf
+++ main.tf
@@ -6,61 +6,61 @@ provider "aws" {
 
 # Creating VPC
 
-resource "aws_vpc" "tobi-projectvpc" {
+resource "aws_vpc" "tdex-projectvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "tobi-project"
+    Name = "tdex-project"
   }
 }
 
 #Creating Subnet
 
-resource "aws_subnet" "tobi-project_sub" {
-  vpc_id     = aws_vpc.tobi-projectvpc.id
+resource "aws_subnet" "tdex-project_sub" {
+  vpc_id     = aws_vpc.tdex-projectvpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "tobi-project"
+    Name = "tdex-project"
   }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "tobi-project_gw" {
-  vpc_id = aws_vpc.tobi-projectvpc.id
+resource "aws_internet_gateway" "tdex-project_gw" {
+  vpc_id = aws_vpc.tdex-projectvpc.id
 
   tags = {
-    Name = "tobi-project"
+    Name = "tdex-project"
   }
 }
 
-resource "aws_route_table" "tobi-project_route" {
-  vpc_id = aws_vpc.tobi-projectvpc.id
+resource "aws_route_table" "tdex-project_route" {
+  vpc_id = aws_vpc.tdex-projectvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tobi-project_gw.id
+    gateway_id = aws_internet_gateway.tdex-project_gw.id
   }
 
   tags = {
-    Name = "tobi-project"
+    Name = "tdex-project"
   }
 }
 
 #Security group
 
-resource "aws_security_group" "tobi-project_sg" {
+resource "aws_security_group" "tdex-project_sg" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.tobi-projectvpc.id
+  vpc_id      = aws_vpc.tdex-projectvpc.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.tobi-projectvpc.cidr_block]
+    cidr_blocks      = [aws_vpc.tdex-projectvpc.cidr_block]
   }
 
   egress {
@@ -76,19 +76,19 @@ resource "aws_security_group" "tobi-project_sg" {
   }
 }
 
-resource "aws_network_interface" "tobi-project_ni" {
-  subnet_id       = aws_subnet.tobi-project_sub.id
-  security_groups = [aws_security_group.tobi-project_sg.id]
+resource "aws_network_interface" "tdex-project_ni" {
+  subnet_id       = aws_subnet.tdex-project_sub.id
+  security_groups = [aws_security_group.tdex-project_sg.id]
 
   
 
 }
 
-resource "aws_instance" "tobi-project_web" {
+resource "aws_instance" "tdex-project_web" {
   ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "tobi-project"
+    Name = "tdex-project"
   }
 }
