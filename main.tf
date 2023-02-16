fb4487de9001b774de7519909e748e2fbf4decfb
tdexpeter <117410568+tdexpeter@users.noreply.github.com>
2023-02-16 00:50:47 -0600

    push 2

--- main.tf
+++ main.tf
@@ -6,61 +6,61 @@ provider "aws" {
 
 # Creating VPC
 
-resource "aws_vpc" "open-react-templatevpc" {
+resource "aws_vpc" "tobi-projectvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "open-react-template"
+    Name = "tobi-project"
   }
 }
 
 #Creating Subnet
 
-resource "aws_subnet" "open-react-template_sub" {
-  vpc_id     = aws_vpc.open-react-templatevpc.id
+resource "aws_subnet" "tobi-project_sub" {
+  vpc_id     = aws_vpc.tobi-projectvpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "open-react-template"
+    Name = "tobi-project"
   }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "open-react-template_gw" {
-  vpc_id = aws_vpc.open-react-templatevpc.id
+resource "aws_internet_gateway" "tobi-project_gw" {
+  vpc_id = aws_vpc.tobi-projectvpc.id
 
   tags = {
-    Name = "open-react-template"
+    Name = "tobi-project"
   }
 }
 
-resource "aws_route_table" "open-react-template_route" {
-  vpc_id = aws_vpc.open-react-templatevpc.id
+resource "aws_route_table" "tobi-project_route" {
+  vpc_id = aws_vpc.tobi-projectvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.open-react-template_gw.id
+    gateway_id = aws_internet_gateway.tobi-project_gw.id
   }
 
   tags = {
-    Name = "open-react-template"
+    Name = "tobi-project"
   }
 }
 
 #Security group
 
-resource "aws_security_group" "open-react-template_sg" {
+resource "aws_security_group" "tobi-project_sg" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.open-react-templatevpc.id
+  vpc_id      = aws_vpc.tobi-projectvpc.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.open-react-templatevpc.cidr_block]
+    cidr_blocks      = [aws_vpc.tobi-projectvpc.cidr_block]
   }
 
   egress {
@@ -76,19 +76,19 @@ resource "aws_security_group" "open-react-template_sg" {
   }
 }
 
-resource "aws_network_interface" "open-react-template_ni" {
-  subnet_id       = aws_subnet.open-react-template_sub.id
-  security_groups = [aws_security_group.open-react-template_sg.id]
+resource "aws_network_interface" "tobi-project_ni" {
+  subnet_id       = aws_subnet.tobi-project_sub.id
+  security_groups = [aws_security_group.tobi-project_sg.id]
 
   
 
 }
 
-resource "aws_instance" "open-react-template_web" {
+resource "aws_instance" "tobi-project_web" {
   ami           = "ami-0b5eea76982371e91"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "open-react-template"
+    Name = "tobi-project"
   }
 }
