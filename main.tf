aa83b0b14ac7ec56e79e49f6683dd7d3474bc52e
tdexpeter <117410568+tdexpeter@users.noreply.github.com>
2023-02-23 18:24:02 -0600

    push 12

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  access_key = [AWS_ACCESS_KEY_ID]
+  secret_key = [AWS_SECRET_ACCESS_KEY]
 }
 
 # Creating VPC
