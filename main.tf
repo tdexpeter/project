7045f4671bb510939e5b19c4481255340662c137
tdexpeter <117410568+tdexpeter@users.noreply.github.com>
2023-02-23 18:56:25 -0600

    safe keys

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = {{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = {{ secrets.AWS_SECRET_ACCESS_KEY }}
+  aws_access_key = "secrets.AWS_ACCESS_KEY_ID"
+  aws_secret_key = "secrets.AWS_SECRET_ACCESS_KEY"
 }
 
 # Creating VPC
